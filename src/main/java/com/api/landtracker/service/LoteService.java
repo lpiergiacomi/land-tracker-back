8ebb56334a5fe97224a6035ab75ed34706206263
Lucas Piergiacomi <lucas@gestioncervecera.com>
2023-09-08 01:02:57 -0300

    modificado data.json + transactional en guardarlote

--- src/main/java/com/api/landtracker/service/LoteService.java
+++ src/main/java/com/api/landtracker/service/LoteService.java
@@ -5,9 +5,9 @@ import com.api.landtracker.model.entities.Lote;
 import com.api.landtracker.model.mappers.LoteMapper;
 import com.api.landtracker.repository.LoteRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +23,7 @@ public class LoteService {
         List<LoteDTO> loteDTOS = loteMapper.lotesToLotesDTO(lotes);
         return loteDTOS;
     }
+    @Transactional
     public LoteDTO guardarLote(LoteDTO loteDTO) {
         Lote lote = loteMapper.loteDTOToLote(loteDTO);
         LoteDTO loteResponse = loteMapper.loteToLoteDTO(loteRepository.save(lote));
@@ -30,7 +31,8 @@ public class LoteService {
     }
 
     public LoteDTO obtenerLotePorId(Long id) {
-        Lote lote = loteRepository.getReferenceById(id);
+        Lote lote = loteRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("No se encontró un lote con ese id"));
         LoteDTO loteDTO = loteMapper.loteToLoteDTO(lote);
         return loteDTO;
     }
