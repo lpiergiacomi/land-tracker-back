e69ec1106de6629c330ae41fc18ac8199dd73c6a
nahuelmpereyra <nahuelmpereyra@gmail.com>
2023-09-13 22:28:46 -0300

    enpoint para filtrar lotes

--- src/main/java/com/api/landtracker/controller/LoteController.java
+++ src/main/java/com/api/landtracker/controller/LoteController.java
@@ -2,17 +2,25 @@ package com.api.landtracker.controller;
 
 import com.api.landtracker.model.dto.LoteDTO;
 import com.api.landtracker.model.entities.Lote;
+import com.api.landtracker.model.filter.LoteFilterParams;
 import com.api.landtracker.service.LoteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/api/lotes")
@@ -31,9 +39,22 @@ public class LoteController {
     public List<LoteDTO> obtenerTodosLosLotes() {
         return loteService.obtenerTodosLosLotes();
     }
-
     @PostMapping
     public LoteDTO guardarLote(@RequestBody LoteDTO lote) {
         return loteService.guardarLote(lote);
     }
+
+    @GetMapping("/filter")
+    public ResponseEntity<Page<LoteDTO>> pages(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "200") int size,
+            @RequestParam(defaultValue = "nombre") String order,
+            @RequestParam(defaultValue = "true") boolean asc,
+            @RequestBody LoteFilterParams movementParams) {
+
+        Page<LoteDTO> movements = loteService.obtenerLotesConFiltro(movementParams,
+                PageRequest.of(page, size, Sort.by(order)));
+        return ok(movements);
+    }
+
 }
--- src/main/java/com/api/landtracker/repository/LoteRepository.java
+++ src/main/java/com/api/landtracker/repository/LoteRepository.java
@@ -2,8 +2,9 @@ package com.api.landtracker.repository;
 
 import com.api.landtracker.model.entities.Lote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoteRepository extends JpaRepository<Lote, Long> {
+public interface LoteRepository extends JpaRepository<Lote, Long>, JpaSpecificationExecutor<Lote> {
 }
