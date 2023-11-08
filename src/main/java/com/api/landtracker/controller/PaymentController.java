3d9197b6b0da41f9f85b5ed3a1ce9cfcee63bec0
Lucas Piergiacomi <lg.piergiacomi@gmail.com>
2023-11-07 23:57:41 -0300

    validacion de lote reservado al agregar pago

--- src/main/java/com/api/landtracker/controller/PaymentController.java
+++ src/main/java/com/api/landtracker/controller/PaymentController.java
@@ -1,10 +1,8 @@
 package com.api.landtracker.controller;
 
 import com.api.landtracker.model.dto.PaymentDTO;
-import com.api.landtracker.model.entities.File;
-import com.api.landtracker.model.entities.ResponseFile;
-import com.api.landtracker.service.FileService;
 import com.api.landtracker.service.PaymentService;
+import com.api.landtracker.utils.exception.DataValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,25 +17,17 @@ public class PaymentController {
 
 
     private final PaymentService paymentService;
-    private final FileService fileService;
 
     @PostMapping
     public PaymentDTO savePayment(@RequestParam(value = "file", required = false) MultipartFile file, @RequestParam("lotId") Long lotId,
                                   @RequestParam("userId") Long userId,
-                                  @RequestParam("amount") BigDecimal amount) throws IOException {
-        File insertedFile = null;
-
-        if (file != null) {
-            ResponseFile responseFile = fileService.store(file, lotId);
-            insertedFile = new File();
-            insertedFile.setId(responseFile.getId());
-        }
+                                  @RequestParam("amount") BigDecimal amount) throws IOException, DataValidationException {
 
         PaymentDTO paymentDTO = new PaymentDTO();
         paymentDTO.setUserId(userId);
         paymentDTO.setLotId(lotId);
         paymentDTO.setAmount(amount);
 
-        return this.paymentService.savePayment(paymentDTO, insertedFile);
+        return this.paymentService.savePayment(paymentDTO, file);
     }
 }
--- src/main/java/com/api/landtracker/service/PaymentService.java
+++ src/main/java/com/api/landtracker/service/PaymentService.java
@@ -1,16 +1,19 @@
 package com.api.landtracker.service;
 
 import com.api.landtracker.model.dto.PaymentDTO;
-import com.api.landtracker.model.entities.File;
-import com.api.landtracker.model.entities.Payment;
+import com.api.landtracker.model.entities.*;
 import com.api.landtracker.model.mappers.PaymentMapper;
+import com.api.landtracker.repository.LotRepository;
 import com.api.landtracker.repository.PaymentRepository;
 import com.api.landtracker.repository.ReserveRepository;
+import com.api.landtracker.utils.exception.DataValidationException;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 @Service
@@ -18,20 +21,40 @@ import java.time.LocalDate;
 public class PaymentService {
 
     private final PaymentRepository paymentRepository;
-    private final PaymentMapper paymentMapper;
     private final ReserveRepository reserveRepository;
+    private final LotRepository lotRepository;
+    private final PaymentMapper paymentMapper;
     private final EntityManager entityManager;
+    private final FileService fileService;
 
     @Transactional(rollbackOn = Exception.class)
-    public PaymentDTO savePayment(PaymentDTO payment, File file) {
+    public PaymentDTO savePayment(PaymentDTO payment, MultipartFile file) throws IOException, DataValidationException {
+        this.validateLotState(payment.getLotId());
+        File insertedFile = null;
+
+        if (file != null) {
+            ResponseFile responseFile = fileService.store(file, payment.getLotId());
+            insertedFile = new File();
+            insertedFile.setId(responseFile.getId());
+        }
+
         payment.setClientId(this.reserveRepository.findByLotId(payment.getLotId()).getClient().getId());
         Payment paymentToSave = this.paymentMapper.paymentDTOToPayment(payment);
-        paymentToSave.setFile(file);
+        paymentToSave.setFile(insertedFile);
         paymentToSave.setCreatedDate(LocalDate.now());
 
         Payment insertedPayment = this.paymentRepository.saveAndFlush(paymentToSave);
         this.entityManager.refresh(insertedPayment);
         Payment response = this.paymentRepository.findById(insertedPayment.getId()).orElseThrow();
         return this.paymentMapper.paymentToPaymentDTO(response);
+    }
+
+    private void validateLotState(Long lotId) throws DataValidationException {
+        Lot lot = this.lotRepository.findById(lotId).orElseThrow(
+                () -> new DataValidationException("No se encontró el lote"));
+
+        if (lot.getState() != LotState.RESERVADO) {
+            throw new DataValidationException("El lote no se encuentra en un estado para añadirle un pago");
+        }
     }
 }
