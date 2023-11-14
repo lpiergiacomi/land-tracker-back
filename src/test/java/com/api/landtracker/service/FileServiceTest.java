ccc4d6f911629d342d0566772a1d9d419ec31726
nahuelmpereyra <nahuelmpereyra@gmail.com>
2023-11-14 20:12:47 -0300

    mas tests en FileService

--- src/test/java/com/api/landtracker/service/FileServiceTest.java
+++ src/test/java/com/api/landtracker/service/FileServiceTest.java
@@ -12,9 +12,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -47,5 +52,81 @@ class FileServiceTest {
         assertEquals("test.txt", responseFile.getName());
         assertEquals("UnId", responseFile.getId());
         assertEquals("text/plain", responseFile.getType());
-        assertEquals("Prueba inicial".getBytes().length, responseFile.getSize());    }
+        assertEquals("Prueba inicial".getBytes().length, responseFile.getSize());
+    }
+
+    @Test
+    void testGetFile() {
+        String fileId = "test-file-id";
+        File expectedFile = new File();
+        expectedFile.setId(fileId);
+
+        when(fileRepository.findById(fileId)).thenReturn(Optional.of(expectedFile));
+
+        File result = fileService.getFile(fileId);
+
+        verify(fileRepository).findById(fileId);
+        assertEquals(expectedFile, result);
+    }
+
+    @Test
+    void testGetFile_RecordNotFound() {
+        String fileId = "non-existent-file-id";
+
+        when(fileRepository.findById(fileId)).thenReturn(Optional.empty());
+
+        assertThrows(RecordNotFoundHttpException.class, () -> fileService.getFile(fileId));
+        verify(fileRepository).findById(fileId);
+    }
+
+    @Test
+    void testGetAllFiles() {
+        File file1 = new File();
+        File file2 = new File();
+
+        when(fileRepository.findAll()).thenReturn(List.of(file1, file2));
+
+        Stream<File> result = fileService.getAllFiles();
+
+        assertEquals(List.of(file1, file2), result.collect(Collectors.toList()));
+        verify(fileRepository).findAll();
+    }
+
+    @Test
+    void testGetListFilesByLotId() {
+        Long lotId = 1L;
+        File file1 = new File();
+        File file2 = new File();
+
+        when(fileRepository.findByLotId(lotId)).thenReturn(List.of(file1, file2));
+
+        Stream<File> result = fileService.getListFilesByLotId(lotId);
+
+        assertEquals(List.of(file1, file2), result.collect(Collectors.toList()));
+        verify(fileRepository).findByLotId(lotId);
+    }
+
+    @Test
+    void testDeleteFile() {
+        String fileId = "test-file-id";
+        File fileToDelete = new File();
+
+        when(fileRepository.findById(fileId)).thenReturn(Optional.of(fileToDelete));
+
+        fileService.deleteFile(fileId);
+
+        verify(fileRepository).findById(fileId);
+        verify(fileRepository).deleteById(fileId);
+    }
+
+    @Test
+    void testDeleteFile_RecordNotFound() {
+        String fileId = "non-existent-file-id";
+
+        when(fileRepository.findById(fileId)).thenReturn(Optional.empty());
+
+        assertThrows(RecordNotFoundHttpException.class, () -> fileService.deleteFile(fileId));
+        verify(fileRepository).findById(fileId);
+    }
+
 }
