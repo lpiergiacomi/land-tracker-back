34848aba6948fe7f823b023721cc4d5c4334d526
nahuelmpereyra <nahuelmpereyra@gmail.com>
2023-11-30 19:37:54 -0300

    test de reserve controller

--- src/test/java/com/api/landtracker/controller/ReserveControllerTest.java
+++ src/test/java/com/api/landtracker/controller/ReserveControllerTest.java
@@ -1,8 +1,10 @@
 package com.api.landtracker.controller;
 
+import com.api.landtracker.model.dto.LotDTO;
 import com.api.landtracker.model.dto.ReserveDTO;
 import com.api.landtracker.model.entities.ReserveState;
 import com.api.landtracker.service.ReserveService;
+import com.api.landtracker.utils.exception.DataValidationException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,17 +13,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -65,6 +71,28 @@ public class ReserveControllerTest {
                 .andExpect(jsonPath("$.clientName", is("Client 1")));
 
         verify(reserveService, times(1)).saveReserve(any(ReserveDTO.class));
+    }
+
+    @Test
+    void testCancel() throws Exception {
+        Long reserveId = 1L;
+        Long lotId = 1L;
+        Long userId = 1L;
+        LotDTO expectedResult = new LotDTO();
+        expectedResult.setId(1L);
+
+        when(reserveService.cancel(reserveId, lotId, userId)).thenReturn(expectedResult);
+
+        mockMvc.perform(put("/reserves/cancel/{reserveId}", reserveId)
+                        .param("lotId", String.valueOf(lotId))
+                        .param("userId", String.valueOf(userId))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(expectedResult.getId()));
+
+        verify(reserveService, times(1)).cancel(reserveId, lotId, userId);
+
     }
 
     private static String asJsonString(final Object obj) {
